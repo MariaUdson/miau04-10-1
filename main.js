b1e79db837845ec0c50464ffc7d5210bef251ba7
MariaUdson <129086289+MariaUdson@users.noreply.github.com>
2024-10-04 08:17:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+document.querySelector('.pararmetro-senha_botao');
+const numeroSenha = document.querySelector('.parametro-senha_texto');
+let tamanhoSenha = 12;
+senha.TextContent = tamanhoSenha;
+const botoes = document.querySelectorAll('.parametro-senha_botao');
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+console.log(botoes);
+
+function diminuiTamanho() {
+    if(tamanhoSenha > 1)
+    tamanhoSenha = tamanhoSenha - 1;
+    numeroSenha.TextContent = tamanhoSenha;
+}
+function aumentaTamanho() {
+    if (tamanhoSenha < 20){
+    tamanhoSenha = tamanhoSenha + 1;
+    }
+    numeroSenha.TextContent = tamanhoSenha;
+}
+//código omitido
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+geraSenha();
+
+function geraSenha(){
+    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+}
+campoSenha.value = letrasMaiusculas;
